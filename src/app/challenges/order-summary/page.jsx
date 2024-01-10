d3a9fc12dbd3b81951f3981997ff11f90e904bc1
Josh <hello@joshpickard.me>
2024-01-10 12:06:53 +0000

    adding fem link to challenge page

--- src/app/challenges/order-summary/page.jsx
+++ src/app/challenges/order-summary/page.jsx
@@ -15,7 +15,7 @@ export default function order() {
         <Image className="absolute min-w-full min-h-[437px] z-0 h-[437px] hidden sm:block" src={'/order-summary/pattern-background-desktop.svg'} height={100} width={100}></Image>
         <Image className="absolute min-w-full min-h-[194px] z-0 h-[194px] block sm:hidden" src={'/order-summary/pattern-background-desktop.svg'} height={100} width={100}></Image>
         <main className="flex  bg-order-pale-blue  min-w-full min-h-screen justify-center items-center">
-            <Back></Back>
+            <Back fem={'https://www.frontendmentor.io/solutions/order-summary-component-with-entrance-antimation-r6nDsskZ6v'}></Back>
             <Card></Card>
         </main>
         </>

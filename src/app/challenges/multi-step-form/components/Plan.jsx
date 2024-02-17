ae2f9dababc64b4e9db9ea15888a32f66faf6619
Josh <hello@joshpickard.me>
2024-02-17 11:12:19 +0000

    Planform

--- src/app/challenges/multi-step-form/components/Plan.jsx
+++ src/app/challenges/multi-step-form/components/Plan.jsx
@@ -1,15 +1,15 @@
 // Import
 import Image from "next/image"
 
-export default function Plan({icon, title, price}) {
+export default function Plan({icon, title, price, duration}) {
     return (
         <button className="flex text-left flex-row lg:flex-col gap-[14px] outline outline-2 outline-multi-step-border-color hover:outline-multi-step-purple lg:w-[138px] rounded-[8px] p-3 lg:p-4 lg:gap-[39px]">
             <Image src={icon} width={40} height={40}></Image>
-            <div>
+            <div className="flex flex-col gap-[2px] h-full">
                 <h3 className=" font-semibold text-multi-step-denim text-[16px]">{title}</h3>
                 <p className=" text-multi-step-gray text-[14px]">{price}</p>
+                {duration == 'Yearly' && <p className=" text-multi-step-denim text-[12px] font-medium">2 months free</p>}
             </div>
-
         </button>
     )
 }
--- src/app/challenges/multi-step-form/components/PlanForm.jsx
+++ src/app/challenges/multi-step-form/components/PlanForm.jsx
@@ -20,9 +20,9 @@ export default function PlanForm() {
     return (
         <div className="flex flex-col gap-8">
             <div className="flex flex-col lg:flex-row gap-[18px]">
-                <Plan icon={'/multi-step-form/icon-arcade.svg'} title={'Arcade'} price={duration == 'Monthly' ? '$9/mo' : '$90/yr'}></Plan>
-                <Plan icon={'/multi-step-form/icon-advanced.svg'} title={'Advanced'} price={duration == 'Monthly' ? '$12/mo' : '$120/yr'}></Plan>
-                <Plan icon={'/multi-step-form/icon-pro.svg'} title={'Pro'} price={duration == 'Monthly' ? '$15/mo' : '$150/yr'}></Plan>
+                <Plan icon={'/multi-step-form/icon-arcade.svg'} title={'Arcade'} price={duration == 'Monthly' ? '$9/mo' : '$90/yr'} duration={duration} ></Plan>
+                <Plan icon={'/multi-step-form/icon-advanced.svg'} title={'Advanced'} price={duration == 'Monthly' ? '$12/mo' : '$120/yr'} duration={duration} ></Plan>
+                <Plan icon={'/multi-step-form/icon-pro.svg'} title={'Pro'} price={duration == 'Monthly' ? '$15/mo' : '$150/yr'} duration={duration} ></Plan>
             </div>
             <div className="flex w-full h-12 gap-6 rounded-[8px] items-center justify-center bg-multi-step-very-light-gray">
                 <p className={`${duration == 'Monthly' ? 'text-multi-step-denim' : 'text-multi-step-gray'} text-[14px] font-medium`}>Monthly</p>

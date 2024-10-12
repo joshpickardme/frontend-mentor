40af23b32269e61ac7b7f77401bed183abe4e686
Turbobot <turbobot@vercel.com>
2024-10-12 22:39:43 +0100

    feat(create-turbo): apply official-starter transform

--- src/app/challenges/interactive-rating/components/Selection.jsx
+++
@@ -1,9 +0,0 @@
-export default function Selection({ score }) {
-  return (
-    <div className="flex items-center justify-center rounded-full font-medium pl-2 pr-2 pt-1 pb-1 w-[193px] h-[32px] bg-rating-dark-blue ">
-      <p className="text-rating-orange text-[15px]">
-        You selected {score} out of 5
-      </p>
-    </div>
-  );
-}
